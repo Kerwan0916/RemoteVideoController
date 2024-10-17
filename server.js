27d750a596948b0468c119ec16885a61031b48d1
Kerwan0916 <jkevin.fuss@gmail.com>
2024-10-17 19:40:08 -0400

    fixed bugs and started working on executable

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+// const open = require('open');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,11 +16,18 @@ io.on('connection', (socket) => {
         io.emit('playVideo');
     });
 
+    socket.on('pauseVideo', () => {
+        io.emit('pauseVideo');
+      });
+
     socket.on('disconnect', () => {
         console.log('A user disconnected');
     });
 });
 
+// add async after the comma after 3000 to enable the open
 server.listen(3000, () => {
     console.log('Server is running on http://localhost3000');
+    // Automatically open video.html in the browser
+    //await open('http://localhost:3000/video.html');
 });
